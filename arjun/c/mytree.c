2be1680312eda2dd2a3708109c82730835e47e1a
ARC292 <147056197+ARC292@users.noreply.github.com>
2023-11-21 10:18:10 +0530

    Add files via upload

---
+++ arjun/c/mytree.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct t
+{
+ int data;
+ struct t *l;
+ struct t *r;
+}
+
+struct t *head=NULL;
+
+void ins()
+{
+  if(head==NULL);
+
+  printf("enter the element:");
+  temp=(struct linn *)malloc(sizeof(struct linn));
+      
+ 
+
+}
